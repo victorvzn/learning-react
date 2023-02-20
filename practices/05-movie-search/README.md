5c26642e43c26adbfa76589cf8ad311cdad08402
Victor Villazón <victorzn23@gmail.com>
2023-02-20 15:10:58 -0500

    feat: list movies and show title, year and poster

--- practices/05-movie-search/README.md
+++ practices/05-movie-search/README.md
@@ -9,7 +9,7 @@ Consigue la `API Key` en la propia página web registrando tu email.
 Requerimientos:
 
 - [x] Necesita mostrar un input para buscar la película y un botón para buscar.
-- [ ] Lista las películas y muestra el título, año y poster.
+- [x] Lista las películas y muestra el título, año y poster.
 - [ ] Que el formulario funcione
 - [ ] Haz que las películas se muestren en un grid responsive.
 - [ ] Hacer el fetching de datos a la API
--- practices/05-movie-search/src/App.jsx
+++ practices/05-movie-search/src/App.jsx
@@ -1,6 +1,13 @@
 import './App.css'
 
+import responseMovies from './mocks/with-results.json'
+// import withoutResults from './mocks/no-results.json'
+
 function App () {
+  const movies = responseMovies.Search
+
+  const hasMovies = movies?.length > 0
+
   return (
     <div className='page'>
       <header className='header'>
@@ -13,7 +20,25 @@ function App () {
       </header>
 
       <main className='main'>
-        The results will go here!
+        {
+          hasMovies
+            ? (
+              <ul>
+                {
+                  movies.map(movie => (
+                    <li key={movie.imdbID}>
+                      <h3>{movie.Title}</h3>
+                      <p>{movie.Year}</p>
+                      <img src={movie.Poster} alt={movie.Title} />
+                    </li>
+                  ))
+                }
+              </ul>
+              )
+            : (
+              <p>No movies were found for this search</p>
+              )
+        }
       </main>
     </div>
   )
