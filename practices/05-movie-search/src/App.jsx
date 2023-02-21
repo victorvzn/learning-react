107ce367aa20f01193288dd4e031fe85466ab295
Victor Villazón <victorzn23@gmail.com>
2023-02-20 20:20:56 -0500

    chore: use  window.FormData

--- practices/05-movie-search/src/App.jsx
+++ practices/05-movie-search/src/App.jsx
@@ -6,13 +6,20 @@ import { useMovies } from './hooks/useMovies'
 function App () {
   const { movies } = useMovies()
 
+  const handleSearch = (event) => {
+    event.preventDefault()
+    const fields = new window.FormData(event.target)
+    const query = fields.get('query')
+    console.log(query)
+  }
+
   return (
     <div className='page'>
       <header className='header'>
         <h1>Movie Search</h1>
 
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, Free Guy...' />
+        <form className='form' onSubmit={handleSearch}>
+          <input name='query' placeholder='Avengers, Star Wars, Free Guy...' />
           <button type='submit'>Search</button>
         </form>
       </header>
