43b5395d8534af62f38d4634f7f4b3c4c29f2f40
Victor Villazón <victorzn23@gmail.com>
2023-02-21 08:19:03 -0500

    feat: Prevent the same search twice with useRef

--- practices/05-movie-search/src/hooks/useMovies.js
+++ practices/05-movie-search/src/hooks/useMovies.js
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { fetchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
 
   const searchMovies = async () => {
+    if (search === previousSearch.current) {
+      return
+    }
+
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const newMovies = await fetchMovies({ search })
       setMovies(newMovies)
     } catch (e) {
