95d15738abd9c58a4824abab2c51945c69e80ae6
Victor Villazón <victorzn23@gmail.com>
2023-02-21 15:12:43 -0500

    feat: challenge finished

--- practices/04-react-prueba-tecnica/README.md
+++ practices/04-react-prueba-tecnica/README.md
@@ -4,12 +4,12 @@ Prueba técnica para Juniors y Trainees de React en Live Coding.
 
 APIs:
 
-- [ ] Facts Random: https://catfact.ninja/fact
-- [ ] Imagen random: https://cataas.com/cat/says/hello
+- Facts Random: https://catfact.ninja/fact
+- Imagen random: https://cataas.com/cat/says/hello
 
-- [ ] Recupera un hecho aleatorio de gatos de la primera API
-- [ ] Recuperar la primera palabra del hecho
-- [ ] Muestra una imagen de un gato con la primera palabra.
+- [x] Recupera un hecho aleatorio de gatos de la primera API
+- [x] Recuperar la primera palabra del hecho
+- [x] Muestra una imagen de un gato con la primera palabra.
 - [ ] ¿Qué más harías?
 
 **Source:** [midudev/aprendiendo-react](https://github.com/midudev/aprendiendo-react)
--- practices/04-react-prueba-tecnica/components/App.jsx
+++ practices/04-react-prueba-tecnica/components/App.jsx
@@ -1,3 +1,45 @@
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
 export function App () {
-  return (<h1>App de gatitos!</h1>)
+  const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+  const [imageUrl, setImageUrl] = useState('')
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const twoFirstWords = fact.split(' ', 2).join(' ')
+
+        console.log(twoFirstWords)
+
+        fetch(`https://cataas.com/cat/says/${twoFirstWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(data => {
+            const { url } = data
+            setImageUrl(url)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>App de gatitos!</h1>
+
+      {fact && <p>{fact}</p>}
+
+      {imageUrl &&
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt={`Image extracted using the first two words for ${fact}`}
+        />}
+    </main>
+  )
 }
