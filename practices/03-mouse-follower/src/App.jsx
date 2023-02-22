76531123d6f7c3585726bdfc0e7edb6a7c8b4f75
Victor Villazón <victorzn23@gmail.com>
2023-02-22 18:28:01 -0500

    feat: update mouse follower

--- practices/03-mouse-follower/src/App.jsx
+++ practices/03-mouse-follower/src/App.jsx
@@ -1,6 +1,68 @@
+import { useEffect, useState } from 'react'
+
+function useFolloMouse () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('useEffect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return {
+    enabled,
+    position,
+    setEnabled
+  }
+}
+
+function FollowMouse () {
+  const { enabled, position, setEnabled } = useFolloMouse()
+
+  const handleClick = () => setEnabled(!enabled)
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <h3>Mouse follower</h3>
+      <button onClick={handleClick}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
 function App () {
   return (
-    <h3>Mouse follower</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
