a5df5328c028e072d99b8f08cb672591dfcefa2c
Victor Villazón <victorzn23@gmail.com>
2023-02-25 11:40:33 -0500

    feat: add Step upload componente base

--- practices/998-remove-bg-cloudinary/src/App.svelte
+++ practices/998-remove-bg-cloudinary/src/App.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import CloudinaryLogo from './CloudinaryLogo.svelte'
+    import StepUpload from './StepUpload.svelte';
 </script>
 
-<div class="max-w-xl m-auto grid place-content-center w-full h-screen">
+<div class="max-w-xl m-auto grid grid-cols-1 place-content-center w-full h-screen p-4">
   <header class="flex justify-center py-10">
     <h1 class="text-3xl font-bold text-blue-900 tracking-tighter">villa<span class="text-blue-600">remove</span><span class="text-blue-500">bg</span></h1>
   </header>
 
   <main>
-    Contenido
+    <StepUpload />
   </main>
   
-  <footer class="flex justify-center items-center gap-x-2 font-semi pt-10">
-    Hecho con <a href="https://cloudinary.com" target="_blank" rel="noreferrer"><CloudinaryLogo /></a> and <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+  <footer class="flex justify-center items-center gap-x-2 font-semibold pt-10">
+    Hecho con <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" /> and <a href="https://cloudinary.com" target="_blank" rel="noreferrer"><CloudinaryLogo /></a>
   </footer>
 </div>
---
+++ practices/998-remove-bg-cloudinary/src/StepUpload.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Dropzone from 'dropzone'
+  import 'dropzone/dist/dropzone.css'
+
+  import { onMount } from 'svelte';
+</script>
+
+<form
+  id="dropzone"
+  class="shadow-2xl border-dashed border-2 border-gray-300 rounded-lg aspect-video flex items-center justify-center flex-col">
+  <button
+    class="pointer-events-none bg-blue-600 rounded-full text-bold text-white text-xl px-6 py-4">
+    Upload files
+  </button>
+</form>
