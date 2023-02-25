9765d3b1a3b685ba3d28d931048ccff56f3f214f
Victor Villazón <victorzn23@gmail.com>
2023-02-25 11:16:42 -0500

    feat: add main layout with logo, content and footer + tailwind

--- practices/998-remove-bg-cloudinary/src/App.svelte
+++ practices/998-remove-bg-cloudinary/src/App.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import CloudinaryLogo from './CloudinaryLogo.svelte'
 </script>
 
-<h1>villaremove.bg</h1>
+<div class="max-w-xl m-auto grid place-content-center w-full h-screen">
+  <header class="flex justify-center py-10">
+    <h1 class="text-3xl font-bold text-blue-900 tracking-tighter">villa<span class="text-blue-600">remove</span><span class="text-blue-500">bg</span></h1>
+  </header>
+
+  <main>
+    Contenido
+  </main>
+  
+  <footer class="flex justify-center items-center gap-x-2 font-semi pt-10">
+    Hecho con <a href="https://cloudinary.com" target="_blank" rel="noreferrer"><CloudinaryLogo /></a>
+  </footer>
+</div>
