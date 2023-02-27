83810dbc403f0c6a1fa433a29843470c40211c3a
Victor Villazón <victorzn23@gmail.com>
2023-02-27 12:47:52 -0500

    refactor(996-tdd-buy-some-milk-rectjs-typescript) T02. render another and multiple items

--- practices/996-tdd-buy-dome-milk-reactjs-typescript/src/Todo.test.tsx
+++ practices/996-tdd-buy-dome-milk-reactjs-typescript/src/Todo.test.tsx
@@ -1,16 +1,47 @@
 import { describe, expect, it } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
-function Todo() {
-  return <h1>buy some milk</h1>
+function Todo ({ items }) {
+  return (
+    <>
+      {items.map(item => (
+        <div key={item.id}>{item.text}</div>
+      ))}
+    </>
+  )
 }
 
 describe('Buy some mil app', () => {
   it('renders an item', () => {
-    const todos= ['buy some milk']
+    const items = [{ id: 'item-1', text: 'buy some milk' }]
 
-    render(<Todo />)
+    render(<Todo items={items} />)
 
     screen.getByText('buy some milk')
+  })
+
+  it('renders another item', () => {
+    const items = [{ id: 'item-1', text: 'buy some apples' }]
+
+    render(<Todo items={items} />)
+
+    screen.getByText('buy some apples')
+  })
+
+  it('renders multiple items', () => {
+    const items = [
+      {
+        id: 'item-1',
+        text: 'buy some milk'
+      },
+      {
+        id: 'item-2',
+        text: 'buy some apples'
+      }
+    ]
+
+    render(<Todo items={items} />)
+
+    screen.getByText('buy some apples')
   })
 })
