5ae4025890760ca21df198ddb9e21b5d21f95ee4
Victor Villazón <victorzn23@gmail.com>
2023-03-17 17:45:41 -0500

    feat(08-todo-app-ts): add a mockTodos array

--- practices/08-todo-app-ts/src/App.tsx
+++ practices/08-todo-app-ts/src/App.tsx
@@ -1,3 +1,21 @@
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  }
+]
+
 const App = (): JSX.Element => {
   return (
     <h1>TODO MVC</h1>
