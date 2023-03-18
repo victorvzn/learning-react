8380ae3f67d2b23d8e02d2cc8b268e67876b89fa
Victor Villazón <victorzn23@gmail.com>
2023-03-18 00:04:57 -0500

    feat(08-todo-app-ts): update todos with class completed

--- practices/08-todo-app-ts/src/Todos.tsx
+++ practices/08-todo-app-ts/src/Todos.tsx
@@ -9,7 +9,10 @@ export const Todos: React.FC<Props> = ({ todos }) => {
   return (
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>
+        <li
+          key={todo.id}
+          className={`${todo.completed ? 'completed' : ''}`}
+        >
           {todo.title}
         </li>
       ))}
