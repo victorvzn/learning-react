75d63c3ee8623f4001dca622cdf9c7e2bfbbc28c
Victor Villazón <victorzn23@gmail.com>
2023-04-05 04:32:54 -0500

    feat(09-google-translate-clone-ts): add base reducer

--- practices/09-google-translate-clone-ts/src/App.tsx
+++ practices/09-google-translate-clone-ts/src/App.tsx
@@ -1,7 +1,66 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useReducer } from 'react'
+
+// 1. Create a initialstate
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+// 2. Create a reducer
+function reducer (state, action) {
+  const { type, payload } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: payload
+    }
+  }
+
+  return state
+}
 
 function App () {
+  // 3. Usar el hook useReducer
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className='App'>
       <h1>Google translate</h1>
