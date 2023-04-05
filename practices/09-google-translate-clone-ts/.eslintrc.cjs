329d547a23b8f369f408c3e76cea41ccdbe85168
Victor Villazón <victorzn23@gmail.com>
2023-04-05 03:52:19 -0500

    feat(09-google-translate-clone-ts): install and config linter standard-ts

---
+++ practices/09-google-translate-clone-ts/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    // Parsing error: Cannot read file '.../tsconfig.json'.eslint: --> https://stackoverflow.com/a/64940811
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
