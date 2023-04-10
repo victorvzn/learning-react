fef4a8bff68e69503b03c1860f4a092ff5882756
Victor Villazón <victorzn23@gmail.com>
2023-04-10 12:28:29 -0500

    feat(10-crud-react-redux-ts): handle form errors

--- practices/10-crud-react-redux-ts/src/components/CreateNewUser.tsx
+++ practices/10-crud-react-redux-ts/src/components/CreateNewUser.tsx
@@ -1,8 +1,10 @@
-import { Button, Card, TextInput, Title } from "@tremor/react";
+import { Badge, Button, Card, TextInput, Title } from "@tremor/react";
+import { useState } from "react";
 import { useUserActions } from "../hooks/useUserActions";
 
 export function CreateNewUser() {
 	const { addUser } = useUserActions();
+	const [result, setResult] = useState<"ok" | "ko" | null>(null);
 
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
@@ -14,22 +16,42 @@ export function CreateNewUser() {
 		const email = formData.get("email") as string;
 		const github = formData.get("github") as string;
 
+		if (!name || !email || !github) {
+			return setResult("ko");
+		}
+
 		addUser({ name, email, github });
+		setResult("ok");
+		form.reset();
 	};
 
 	return (
 		<Card style={{ marginTop: "16px" }}>
-			<Title>Create New User</Title>
+			<Title style={{ marginBottom: "8px" }}>Create New User</Title>
 
-			<form onSubmit={handleSubmit}>
+			<form
+				onSubmit={handleSubmit}
+				style={{ display: "flex", flexDirection: "column", gap: "8px" }}
+			>
 				<TextInput name="name" placeholder="Aquí el nombre" />
 				<TextInput name="email" placeholder="Aquí el email" />
 				<TextInput name="github" placeholder="Aquí el usuario de GitHub" />
 
-				<div>
-					<Button type="submit" style={{ marginTop: "16px" }}>
-						Crear usuario
-					</Button>
+				<div
+					style={{
+						display: "flex",
+						alignItems: "center",
+						gap: "8px",
+					}}
+				>
+					<Button type="submit">Crear usuario</Button>
+
+					<span>
+						{result === "ok" && (
+							<Badge color='green'>Guardado correctamente</Badge>
+						)}
+						{result === "ko" && <Badge color='red'>Error con los campos</Badge>}
+					</span>
 				</div>
 			</form>
 		</Card>
