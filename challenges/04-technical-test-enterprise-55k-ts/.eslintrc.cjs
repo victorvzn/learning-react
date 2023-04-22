b460aa8cf25ba6005c84e9b10c3c88420dedb659
Victor Villazón <victorzn23@gmail.com>
2023-04-22 14:03:28 -0500

    feat(04-technical-test-enterprise-55k-ts): config tslint and fix all

---
+++ challenges/04-technical-test-enterprise-55k-ts/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    // Parsing error: Cannot read file '.../tsconfig.json'.eslint: --> https://stackoverflow.com/a/64940811
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
